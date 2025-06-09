703e180993ecce94b844ed3510993c16cf93d6f9
minhlx <minhlx@vetc.com.vn>
2025-06-09 14:14:53 +0700

    Add Dockerfile + env + tmp disable csrf to easy register

--- src/main/java/com/example/accountservices/config/ResourceServerConfig.java
+++ src/main/java/com/example/accountservices/config/ResourceServerConfig.java
@@ -6,9 +6,11 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -26,6 +28,8 @@ public class ResourceServerConfig {
                         // permit all here and secure the endpoints later in controllers
                         .anyRequest().permitAll())
                 .cors(Customizer.withDefaults())
+                // disable CSRF for stateless APIs
+                .csrf(AbstractHttpConfigurer::disable)
                 .oauth2ResourceServer(oauth2 ->
                         oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())));
         return http.build();
